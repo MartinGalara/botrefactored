ee397a5b4a8a41de708267035566abdb123a07fa
Martin Galara <mgalara@gmail.com>
2024-02-22 17:26:48 -0300

    asdasd

--- components/api/apiTickets.js
+++ components/api/apiTickets.js
@@ -1,30 +1,29 @@
-const { downloadMediaMessage } = require('@adiwajshing/baileys')
+const { downloadMediaMessage } = require("@adiwajshing/baileys");
 
-const axios = require('axios')
-const nodemailer = require("nodemailer")
+const axios = require("axios");
+const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
-let ticket = {}
+let ticket = {};
 
-const addProps = (from,props) => {
-    if(ticket.hasOwnProperty(from)){
-      Object.assign(ticket[from], props);
-    }
-    else{
-      ticket[from] = {}
-      Object.assign(ticket[from], props);
-    }
-}
+const addProps = (from, props) => {
+  if (ticket.hasOwnProperty(from)) {
+    Object.assign(ticket[from], props);
+  } else {
+    ticket[from] = {};
+    Object.assign(ticket[from], props);
+  }
+};
 
-const getProp = (from,prop) => {
-    return ticket[from][prop]
-}
+const getProp = (from, prop) => {
+  return ticket[from][prop];
+};
 
 const deleteTicketData = (from) => {
-  ticket[from] = {}
-}
+  ticket[from] = {};
+};
 
 const getInstructivo = (from, index) => {
   const instructivos = ticket[from].instructivos;
@@ -32,12 +31,16 @@ const getInstructivo = (from, index) => {
   // Convertir el índice a un número entero
   const numericIndex = parseInt(index);
 
-  if(parseInt(index) === instructivos.length+1) return "Volver"
-  if(parseInt(index) === instructivos.length+2) return "Salir"
+  if (parseInt(index) === instructivos.length + 1) return "Volver";
+  if (parseInt(index) === instructivos.length + 2) return "Salir";
 
   // Verificar si el índice es un número entero válidoasd
-  if (!Number.isInteger(numericIndex) || numericIndex <= 0 || numericIndex > instructivos.length) {
-    console.error('Índice inválido o fuera de rango.');
+  if (
+    !Number.isInteger(numericIndex) ||
+    numericIndex <= 0 ||
+    numericIndex > instructivos.length
+  ) {
+    console.error("Índice inválido o fuera de rango.");
     return false;
   }
 
@@ -47,129 +50,119 @@ const getInstructivo = (from, index) => {
 };
 
 const getBandera = (from) => {
+  switch (ticket[from].bandera) {
+    case "YP":
+      return "Ingrese su numero de APIES";
 
-  switch (ticket[from].bandera){
+    case "SH":
+      return "Ingrese su numero de identificacion SHELL";
 
-        case "YP": 
-            return "Ingrese su numero de APIES"
+    case "AX":
+      return "Ingrese su numero de identificacion AXION";
 
-        case "SH": 
-            return "Ingrese su numero de identificacion SHELL"
+    case "PU":
+      return "Ingrese su numero de identificacion PUMA";
 
-        case "AX": 
-            return "Ingrese su numero de identificacion AXION"
+    case "GU":
+      return "Ingrese su numero de identificacion GULF";
 
-        case "PU": 
-            return "Ingrese su numero de identificacion PUMA"
+    case "RE":
+      return "Ingrese su numero de identificacion REFINOR";
 
-        case "GU": 
-           return "Ingrese su numero de identificacion GULF"
+    case "BL":
+      return "Ingrese su numero de identificacion";
 
-        case "RE": 
-            return "Ingrese su numero de identificacion REFINOR"
+    case "OT":
+      return "Ingrese su numero de identificacion";
 
-        case "BL": 
-            return "Ingrese su numero de identificacion"
+    default:
+      return;
+  }
+};
 
-        case "OT": 
-            return "Ingrese su numero de identificacion"
+const computerInfo = (from, option) => {
+  if (ticket[from].computers[option - 1] && option !== "0") {
+    const selected = ticket[from].computers[option - 1];
+    const banderaShort =
+      selected.bandera.length > 3
+        ? selected.bandera.substring(0, 3)
+        : selected.bandera;
+    ticket[from].pf = selected.alias;
+    ticket[from].tv = selected.teamviewer_id;
+    ticket[
+      from
+    ].tvalias = `${selected.razonSocial}|${banderaShort}|${selected.identificador}|${selected.ciudad}|${selected.area}|${selected.prefijo}|${selected.extras}`;
+  }
+};
 
-        default:
-          return
-
+const addAudio = async (from, ctx) => {
+  if (!ticket[from].hasOwnProperty("mailAttachments")) {
+    ticket[from].mailAttachments = [];
   }
 
-}
-
-const computerInfo = (from,option) => {
-
-  
-
-  if(ticket[from].computers[option-1] && option !== "0"){
-    const selected = ticket[from].computers[option-1]
-    const banderaShort = selected.bandera.length > 3 ? selected.bandera.substring(0, 3) : selected.bandera;
-    ticket[from].pf = selected.alias
-    ticket[from].tv = selected.teamviewer_id
-    ticket[from].tvalias = `${selected.razonSocial}|${banderaShort}|${selected.identificador}|${selected.ciudad}|${selected.area}|${selected.prefijo}|${selected.extras}`
-  }
-
-}
-
-const addAudio = async (from,ctx) => {
-
-  if(!ticket[from].hasOwnProperty("mailAttachments")){
-    ticket[from].mailAttachments = []
-  }
-
-  const buffer = await downloadMediaMessage(ctx,'buffer')
+  const buffer = await downloadMediaMessage(ctx, "buffer");
 
   const audio = {
-    filename: 'adjunto.mp3',
-    content: Buffer.from(buffer, 'base64')
-  }
-  ticket[from].mailAttachments.push(audio)
+    filename: "adjunto.mp3",
+    content: Buffer.from(buffer, "base64"),
+  };
+  ticket[from].mailAttachments.push(audio);
+};
 
-}
-
-const addImage = async (from,ctx) => {
-
-  if(!ticket[from].hasOwnProperty("mailAttachments")){
-    ticket[from].mailAttachments = []
+const addImage = async (from, ctx) => {
+  if (!ticket[from].hasOwnProperty("mailAttachments")) {
+    ticket[from].mailAttachments = [];
   }
 
-  const buffer = await downloadMediaMessage(ctx,'buffer')
+  const buffer = await downloadMediaMessage(ctx, "buffer");
 
-    const image = {
-      filename: 'adjunto.jpg',
-      content: Buffer.from(buffer, 'base64')
-    }
-    ticket[from].mailAttachments.push(image)
-
-}
+  const image = {
+    filename: "adjunto.jpg",
+    content: Buffer.from(buffer, "base64"),
+  };
+  ticket[from].mailAttachments.push(image);
+};
 
 const incPregunta = (from) => {
-
-  ticket[from].pregunta = ticket[from].pregunta + 1
-
-}
+  ticket[from].pregunta = ticket[from].pregunta + 1;
+};
 
 const incFlagUsers = (from) => {
-
-  ticket[from].flagUsers = ticket[from].flagUsers + 1
-
-}
+  ticket[from].flagUsers = ticket[from].flagUsers + 1;
+};
 
 const sendEmail = async (from) => {
+  const selectedUser = ticket[from].selectedUser;
 
-  const selectedUser = ticket[from].selectedUser
+  const newTicket = await createTicket(selectedUser.id);
 
-  const newTicket = await createTicket(selectedUser.id)
+  await getManagers(from, selectedUser.id);
 
-  await getManagers(from,selectedUser.id)
+  let reciever = "";
 
-  let reciever = ""
-
-  selectedUser.testing === true ? reciever = process.env.TESTINGMAIL : reciever = process.env.RECIEVER
+  selectedUser.testing === true
+    ? (reciever = process.env.TESTINGMAIL)
+    : (reciever = process.env.RECIEVER);
 
   let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
-    secure: true, // true for 465, false for other ports
+    secure: true, // true for 465, false for other portss
     auth: {
       user: process.env.SENDER, // generated ethereal user
       pass: process.env.GMAIL_PASS, // generated ethereal password
     },
   });
 
-  const allEmails = []
+  const allEmails = [];
 
-  ticket[from].selectedUser.email.split(',').map( e => allEmails.push(e))
+  ticket[from].selectedUser.email.split(",").map((e) => allEmails.push(e));
 
-  ticket[from].sendEmail.map( e => allEmails.push(e))
+  ticket[from].sendEmail.map((e) => allEmails.push(e));
 
-  if(selectedUser.vipmail) allEmails.push(selectedUser.vipmail)
+  if (selectedUser.vipmail) allEmails.push(selectedUser.vipmail);
 
-  const replyTo = allEmails.join(', ')
+  const replyTo = allEmails.join(", ");
 
   let data = {
     from: `"WT ${newTicket.id}" <${process.env.SENDER}>`, // sender address
@@ -177,10 +170,13 @@ const sendEmail = async (from) => {
     cc: replyTo,
     subject: `WT ${newTicket.id} | ${selectedUser.info} | Soporte para ${ticket[from].problem} | ${ticket[from].pf}`, // Subject line
     text: `WT ${newTicket.id} | ${selectedUser.info} | Soporte para ${ticket[from].problem} | ${ticket[from].pf}`, // plain text body
-    replyTo: replyTo
-  }
+    replyTo: replyTo,
+  };
 
-  if(ticket[from].mailAttachments && ticket[from].mailAttachments.length !== 0){
+  if (
+    ticket[from].mailAttachments &&
+    ticket[from].mailAttachments.length !== 0
+  ) {
     data.attachments = ticket[from].mailAttachments;
   }
 
@@ -197,74 +193,73 @@ const sendEmail = async (from) => {
   <p>Urgencia indicada por el cliente: ${ticket[from].priority}</p>
 `;
 
-let specificHtml = '';
+  let specificHtml = "";
 
-switch (ticket[from].problem) {
-  case "Despachos CIO":
-  case "Servidor":
-    specificHtml = '';
-    break;
+  switch (ticket[from].problem) {
+    case "Despachos CIO":
+    case "Servidor":
+      specificHtml = "";
+      break;
 
-  case "Sistema SIGES":
-  case "Aplicaciones":
-    specificHtml = `
+    case "Sistema SIGES":
+    case "Aplicaciones":
+      specificHtml = `
     <p>Origen del problema: ${ticket[from].type}</p>
     <p>Descripción del problema: ${ticket[from].description}</p>`;
-    break;
+      break;
 
-  case "Libro IVA":
-    specificHtml = `
+    case "Libro IVA":
+      specificHtml = `
     <p>Solicitud: ${ticket[from].type}</p>
     <p>Período: ${ticket[from].timeFrame}</p>
     <p>Descripción / Info adicional: ${ticket[from].description}</p>`;
-    break;
+      break;
 
-  default:
-    specificHtml = `
+    default:
+      specificHtml = `
     <p>Solicitud: ${ticket[from].type}</p>
     <p>Descripción / Info adicional: ${ticket[from].description}</p>`;
-    break;
-}
+      break;
+  }
 
-data.html = `${commonHtml}${specificHtml}</div>`;
+  data.html = `${commonHtml}${specificHtml}</div>`;
 
   const mail = await transporter.sendMail(data);
 
-  return newTicket.id
-
-}
+  return newTicket.id;
+};
 
 const createTicket = async (clientId) => {
-
   const config = {
-    method: 'post',
+    method: "post",
     url: `${process.env.SERVER_URL}/bottickets`,
-    data:{
-      clientId: clientId
-    }
-  }
+    data: {
+      clientId: clientId,
+    },
+  };
 
-  const ticket = await axios(config)
+  const ticket = await axios(config);
 
-  return ticket.data 
+  return ticket.data;
+};
 
-}
-
-const getManagers = async (from,id) => {
-
+const getManagers = async (from, id) => {
   const config = {
-    method: 'get',
+    method: "get",
     url: `${process.env.SERVER_URL}/clients?id=${id}`,
-  }
+  };
 
-  const managers = await axios(config).then((i) => i.data)
+  const managers = await axios(config).then((i) => i.data);
 
-  ticket[from].sendEmail = []
-  ticket[from].sendMessage = []
+  ticket[from].sendEmail = [];
+  ticket[from].sendMessage = [];
 
   managers.botusers.forEach((botuser) => {
     if (botuser.manager === true) {
-      if ((botuser.email && botuser.area === ticket[from].zone) || botuser.area === "G") {
+      if (
+        (botuser.email && botuser.area === ticket[from].zone) ||
+        botuser.area === "G"
+      ) {
         ticket[from].sendEmail.push(botuser.email);
       }
       if (botuser.area === ticket[from].zone || botuser.area === "G") {
@@ -272,19 +267,20 @@ const getManagers = async (from,id) => {
       }
     }
   });
-}
+};
 
 const sendSosTicket = async (from) => {
+  const selectedUser = ticket[from].selectedUser;
 
-  const selectedUser = ticket[from].selectedUser
+  const newTicket = await createTicket(selectedUser.id);
 
-  const newTicket = await createTicket(selectedUser.id)
+  await getManagers(from, selectedUser.id);
 
-  await getManagers(from,selectedUser.id)
+  let reciever = "";
 
-  let reciever = ""
-
-  selectedUser.testing === true ? reciever = process.env.TESTINGMAIL : reciever = process.env.RECIEVER
+  selectedUser.testing === true
+    ? (reciever = process.env.TESTINGMAIL)
+    : (reciever = process.env.RECIEVER);
 
   let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
@@ -296,23 +292,23 @@ const sendSosTicket = async (from) => {
     },
   });
 
-  const allEmails = []
+  const allEmails = [];
 
-  ticket[from].selectedUser.email.split(',').map( e => allEmails.push(e))
+  ticket[from].selectedUser.email.split(",").map((e) => allEmails.push(e));
 
-  ticket[from].sendEmail.map( e => allEmails.push(e))
+  ticket[from].sendEmail.map((e) => allEmails.push(e));
 
-  if(selectedUser.vipmail) allEmails.push(selectedUser.vipmail)
+  if (selectedUser.vipmail) allEmails.push(selectedUser.vipmail);
 
-  const replyTo = allEmails.join(', ')
+  const replyTo = allEmails.join(", ");
   let data = {
     from: `"WT ${newTicket.id}" <${process.env.SENDER}>`, // sender address
     to: reciever, // list of receivers
     cc: replyTo,
     subject: `WT ${newTicket.id} | *TICKET SOS* | ${selectedUser.info}`, // Subject line
     text: `WT ${newTicket.id} | *TICKET SOS* | ${selectedUser.info}`, // plain text body
-    replyTo: replyTo
-  }
+    replyTo: replyTo,
+  };
 
   data.html = `
   <div>
@@ -325,62 +321,61 @@ const sendSosTicket = async (from) => {
   </div>
   `;
 
-  if(ticket[from].mailAttachments && ticket[from].mailAttachments.length !== 0){
+  if (
+    ticket[from].mailAttachments &&
+    ticket[from].mailAttachments.length !== 0
+  ) {
     data.attachments = ticket[from].mailAttachments;
   }
 
-  console.log(ticket)
+  console.log(ticket);
 
   const mail = await transporter.sendMail(data);
 
-  return newTicket.id
-
-}
+  return newTicket.id;
+};
 
 const getUsers = (from) => {
-
-  if(ticket[from].users.length === 1) {
-    addProps(from,{selectedUser: ticket[from].users[0]})
-    return false
-  }
-  else{
-    const usersString = ticket[from].users.map((user, index) => `${index + 1}. ${user.info}`).join('\n');
+  if (ticket[from].users.length === 1) {
+    addProps(from, { selectedUser: ticket[from].users[0] });
+    return false;
+  } else {
+    const usersString = ticket[from].users
+      .map((user, index) => `${index + 1}. ${user.info}`)
+      .join("\n");
     return usersString;
   }
-  
-}
+};
 
-const newUserProps = (from,props) => {
-
-    Object.assign(ticket[from].newBotuser, props);
-  
-}
+const newUserProps = (from, props) => {
+  Object.assign(ticket[from].newBotuser, props);
+};
 
 const newUserInfo = (from) => {
   const newUser = ticket[from].newBotuser;
 
   // Mapear el valor de 'area' a la palabra completa correspondiente
   const areaMapping = {
-    'P': 'Playa/Boxes',
-    'T': 'Tienda',
-    'A': 'Administracion',
-    'G': 'Gerente / Dueño',
+    P: "Playa/Boxes",
+    T: "Tienda",
+    A: "Administracion",
+    G: "Gerente / Dueño",
   };
 
   // Crear un objeto con los datos formateados
   const formattedData = {
     Nombre: newUser.name,
     Telefono: newUser.phone,
-    'Alta de Usuarios': newUser.createUser ? 'SI' : 'NO',
-    'Ver Instructivos Admin/Contable': newUser.adminPdf ? 'SI' : 'NO',
-    'Generar Ticket SOS': newUser.canSOS ? 'SI' : 'NO',
-    Encargado: newUser.manager ? 'SI' : 'NO',
+    "Alta de Usuarios": newUser.createUser ? "SI" : "NO",
+    "Ver Instructivos Admin/Contable": newUser.adminPdf ? "SI" : "NO",
+    "Generar Ticket SOS": newUser.canSOS ? "SI" : "NO",
+    Encargado: newUser.manager ? "SI" : "NO",
     Area: newUser.manager ? areaMapping[newUser.area] : undefined,
     Correo: newUser.manager ? newUser.email : undefined,
   };
 
   // Crear el string formateado
-  let result = 'Datos Nuevo Usuario\n';
+  let result = "Datos Nuevo Usuario\n";
   for (const [key, value] of Object.entries(formattedData)) {
     if (value !== undefined) {
       result += `${key}: ${value}\n`;
@@ -392,5 +387,20 @@ const newUserInfo = (from) => {
   return result;
 };
 
-
-module.exports = { newUserInfo,newUserProps,addProps,getProp,deleteTicketData,getInstructivo,getBandera,computerInfo,addAudio,addImage,incPregunta,sendEmail,sendSosTicket,incFlagUsers,getUsers }
+module.exports = {
+  newUserInfo,
+  newUserProps,
+  addProps,
+  getProp,
+  deleteTicketData,
+  getInstructivo,
+  getBandera,
+  computerInfo,
+  addAudio,
+  addImage,
+  incPregunta,
+  sendEmail,
+  sendSosTicket,
+  incFlagUsers,
+  getUsers,
+};
